ebde4f031f084dc0833432696ff31941130c7871
bhuaxia@qq.com <bhuaxia@qq.com>
2018-10-01 23:39:36 +0800

    导出功能

--- Vivo.web/Areas/MP/Controllers/PrizeResultController.cs
+++ Vivo.web/Areas/MP/Controllers/PrizeResultController.cs
@@ -28,51 +28,79 @@ namespace Vivo.web.Areas.MP.Controllers
         private IQueryable<PrizeResultInfo> GetListData()
         {
             var list = PrizeResultBLL.GetList(p => true);
+            string RS = Function.GetRequestString("RS");
+            DateTime DateBegin = Function.GetRequestDateTime("DateBegin");
+            DateTime DateEnd = Function.GetRequestDateTime("DateEnd");
+            string Name = Function.GetRequestString("Name");
+            string SN = Function.GetRequestString("SN");
+            if (DateBegin > DicInfo.DateZone)
+            {
+                list = list.Where(a => DbFunctions.DiffDays(a.CreateDate, DateBegin) <= 0);
+                ViewBag.TxtDateBegin = DateBegin.ToString("yyyy-MM-dd");
+            }
+            if (DateEnd > DicInfo.DateZone)
+            {
+                list = list.Where(a => DbFunctions.DiffDays(a.CreateDate, DateEnd) >= 0);
+                ViewBag.TxtDateEnd = DateEnd.ToString("yyyy-MM-dd");
+            }
+            if (!string.IsNullOrEmpty(Name))
+            {
+                list = list.Where(a => a.Name.Contains(Name) || a.Tel.Contains(Name));
+                ViewBag.TxtName = Name;
+            }
+            if (!string.IsNullOrEmpty(RS))
+            {
+                list = list.Where(a => a.IP.Contains(RS));
+                ViewBag.TxtRS = RS;
+            }
+            if (!string.IsNullOrEmpty(SN))
+            {
+                list = list.Where(a => a.SnNumber.Contains(SN));
+                ViewBag.TxtSN = SN;
+            }
 
             list = list.OrderBy(p => p.Result);
             return list;
         }
 
-        //public ActionResult Export()
-        //{
-        //    IQueryable<PrizeResultInfo> list = GetListData();
-        //    DataTable dt = new DataTable();
-        //    dt.Columns.Add("ID");
-        //    dt.Columns.Add("OpenID");
-        //    dt.Columns.Add("姓名");
-        //    dt.Columns.Add("电话");
-        //    dt.Columns.Add("SN号");
-        //    dt.Columns.Add("抽奖时间");
-        //    dt.Columns.Add("抽奖结果");
-        //    dt.Columns.Add("限定活动");
-        //    foreach (var item in list)
-        //    {
+        public ActionResult Export()
+        {
+            IQueryable<PrizeResultInfo> list = GetListData();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("RS");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("StoreAdd");
+            dt.Columns.Add("Tel");
+            dt.Columns.Add("SnNumber");
+            dt.Columns.Add("AreaName");
+            dt.Columns.Add("CreateDate");
+            dt.Columns.Add("Result");
+            foreach (var item in list)
+            {
 
-        //        DataRow dr = dt.NewRow();
-        //        dr["ID"] = item.ID;
-        //        dr["OpenID"] = item.OpenID;
-        //        dr["姓名"] = item.Name;
-        //        dr["电话"] = item.Tel;
-        //        dr["SN号"] = item.SnNumber;
-        //        dr["抽奖时间"] = item.CreateDate;
-        //        dr["抽奖结果"] = item.Result;
-        //        dr["限定活动"] = string.Format("{0}|{1}|{2}到{3}",
-        //             item.PlanInfo.AgenterName,
-        //             item.PlanInfo.MarketLevel,
-        //             item.PlanInfo.DateBegin.ToString("MM-dd HH"),
-        //             item.PlanInfo.DateEnd.ToString("MM-dd HH"));
-        //        dt.Rows.Add(dr);
-        //    }
-        //    string FilePathRelative = "/Content/Export/PrizeResult/";
-        //    string FilePathAbs = Server.MapPath(FilePathRelative);
-        //    string FileName = DateTime.Now.ToFileTime().ToString()+".xlsx";
-        //    if (!Directory.Exists(FilePathAbs))
-        //    {
-        //        Directory.CreateDirectory(FilePathAbs);
-        //    }
-        //    NPOIHelper.Export(dt, FilePathAbs + FileName);
-        //    return Redirect(FilePathRelative + FileName);
-        //}
+                DataRow dr = dt.NewRow();
+                dr["ID"] = item.ID;
+                dr["RS"] = item.IP;
+                dr["Name"] = item.Name;
+                dr["StoreAdd"] = item.StoreAdd;
+                dr["Tel"] = item.Tel;
+                dr["SnNumber"] = item.SnNumber;
+                dr["AreaName"] = item.AreaName;
+                dr["CreateDate"] = item.CreateDate;
+                dr["Result"] = item.Result;
+                dt.Rows.Add(dr);
+            }
+            string FilePathRelative = "/Content/Export/PrizeResult/";
+            string FilePathAbs = Server.MapPath(FilePathRelative);
+            string FileName = DateTime.Now.ToFileTime().ToString() + ".xlsx";
+            if (!Directory.Exists(FilePathAbs))
+            {
+                Directory.CreateDirectory(FilePathAbs);
+            }
+            NPOIHelper.Export(dt, FilePathAbs + FileName);
+            return Redirect(FilePathRelative + FileName);
+        }
 
 
     }
